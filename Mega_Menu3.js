ee401e13f14a27852d0ac833ca7e4aac19a797ff
Fabio-byte <134287684+Fabio-byte@users.noreply.github.com>
2023-06-08 22:20:37 +0200

    Add files via upload
    Aggiunti la maggioranza di file

---
+++ Mega_Menu3.js
@@ -0,0 +1,343 @@
+function scriviMenu(){
+
+    document.getElementById('display').innerHTML = '<nav class="navbar">\
+    <a href="Index.html"\
+       class="brand">Indice</a>\
+    <div class="menu-container">\
+        <ul class="mega-menu">\
+            <li class="dropdown">\
+                <div>\
+                    <span>Vecchio Testamento</span>\
+                    <span class="material-symbols-outlined">\
+                                chevron_down\
+                    </span>\
+                </div>\
+                <ul class="menu">\
+                    <li>\
+                        <a href="#">Vecchio Testamento</a>\
+                    </li>\
+                    <li class="sub-dropdown">\
+                        <div>\
+                            <span>Pentateuco</span>\
+                            <span class="material-symbols-outlined">\
+                                chevron_down\
+                            </span>\
+                        </div>\
+                        <ul class="sub-menu">\
+                            <li>\
+                                <a href="Pentateuco.html"><h3>Pentateuco</h3></a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_01_Genesi.html">Genesi</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_02_Esodo.html">Esodo</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_03_Levitico.html">Levitico</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_04_Numeri.html">Numeri</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_05_Deuteronomio.html">Deuteronomio</a>\
+                            </li>\
+                            </li>\
+                        </ul>\
+                    </li>\
+                    <li class="sub-dropdown">\
+                        <div>\
+                            <span>Storici</span>\
+                            <span class="material-symbols-outlined">\
+                                chevron_right\
+                            </span>\
+                        </div>\
+                        <ul class="sub-menu">\
+                            <li>\
+                                <a href="Storia.html"><h3>Storia</h3></a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_06_Giosuè.html">Giosuè</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_07_Giudici.html">Giudici</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_08_Rut.html">Rut</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_09_1_Samuele.html">Primo libro di Samuele</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_10_2_Samuele.html">Secondo libro di Samuele</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_11_1_Re.html">Primo libro dei Re</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_12_2_Re.html">Secondo libro dei Re</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_13_1_Cronache.html">Primo libro delle Cronache</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_14_2_Cronache.html">Secondo libro delle Cronache</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_15_Esdra.html">Esdra</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_16_Neemia.html">Neemia</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_17_Ester.html">Ester</a>\
+                            </li>\
+                        </ul>\
+                    </li>\
+                    <li class="sub-dropdown">\
+                        <div>\
+                            <span>Poetici e Sapienzali</span>\
+                            <span class="material-symbols-outlined">\
+                                chevron_right\
+                            </span>\
+                        </div>\
+                        <ul class="sub-menu">\
+                            <li>\
+                                <a href="Sapienza.html"><h3>Sapienza</h3></a>\
+                            </li>\
+                                <a href="NR06_18_Giobbe.html">Giobbe</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_19_Salmi.html">Salmi</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_20_Proverbi.html">Proverbi</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_21_Ecclesiaste.html">Ecclesiaste</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_22_Cantico_dei_Cantici.html">Cantico dei Cantici</a>\
+                            </li>\
+                        </ul>\
+                    </li>\
+                    <li class="sub-dropdown">\
+                        <div>\
+                            <span>Profetici Maggiori</span>\
+                            <span class="material-symbols-outlined">\
+                                chevron_right\
+                            </span>\
+                        </div>\
+                        <ul class="sub-menu">\
+                            <li>\
+                                <a href="Profeti.html"><h3>Profeti maggiori</h3></a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_23_Isaia.html">Isaia</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_24_Geremia.html">Geremia</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_25_Lamentazioni.html">Lamentazioni</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_26_Ezechiele.html">Ezechiele</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_27_Daniele.html">Daniele</a>\
+                            </li>\
+                        </ul>\
+                    </li>\
+                    <li class="sub-dropdown">\
+                        <div>\
+                            <span>Profetici Minori</span>\
+                            <span class="material-symbols-outlined">\
+                                chevron_right\
+                            </span>\
+                        </div>\
+                        <ul class="sub-menu">\
+                            <li>\
+                                <a href="Profeti_minori.html"><h3>Profeti minori</h3></a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_28_Osea.html">Osea</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_29_Gioele.html">Gioele</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_30_Amos.html">Amos</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_31_Abdia.html">Abdia</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_32_Giona.html">Giona</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_33_Michea.html">Michea</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_34_Naum.html">Naum</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_35_Abacuc.html">Abacuc</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_36_Sofonia.html">Sofonia</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_37_Aggeo.html">Aggeo</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_38_Zaccaria.html">Zaccaria</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_39_Malachia.html">Malachia</a>\
+                            </li>\
+                        </ul>\
+                    </li>\
+                </ul>\
+            </li>\
+            <!-- Nuovo Testamento -->\
+            <li class="dropdown">\
+                <div>\
+                    <span>Nuovo Testamento</span>\
+                    <span class="material-symbols-outlined">\
+                        chevron_right\
+                    </span>\
+                </div>\
+                <ul class="menu">\
+                    <li>\
+                        <a href="#">Nuovo Testamento</a>\
+                        <li class="sub-dropdown">\
+                        <div>\
+                            <span class="material-symbols-outlined">\
+                                chevron_right\
+                            </span>\
+                        </div>\
+                        <ul class="sub-menu">\
+                            <li>\
+                                <a href="Vangeli.html"><h3>Vangeli</h3></a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_40_Matteo.html">Vangelo di Matteo</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_41_Marco.html">Vangelo di Marco</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_42_Luca.html">Vangelo di Luca</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_43_Giovanni.html">Vangelo di Giovanni</a>\
+                            </li>\
+                            <li>\
+                                <span><a href="NR06_44_Atti.html"><br><br><h3>Atti degli Apostoli</h3></a></span>\
+                            </li>\
+                        </ul>\
+                        </li>\
+                        <li class="sub-dropdown">\
+                        <div>\
+                            <span class="material-symbols-outlined">\
+                                chevron_right\
+                            </span>\
+                        </div>\
+                        <ul class="sub-menu">\
+                            <li>\
+                                <a href="Paolo.html"><h3>Paolo apostolo</h3></a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_45_Romani.html">Lettera ai Romani</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_46_1_Corinzi.html">Prima lettera ai Corinzi</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_47_2_Corinzi.html">Seconda lettera ai Corinzi</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_48_Galati.html">Lettera ai Galati</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_49_Efesini.html">Lettera agli Efesini</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_50_Filippesi.html">Lettera ai Filippesi</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_51_Colossesi.html">Lettera ai Colossesi</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_52_1_Tessalonicesi.html">Prima lettera ai Tessalonicesi</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_53_2_Tessalonicesi.html">Seconda lettera ai Tessalonicesi</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_54_1_Timoteo.html">Prima lettera a Timoteo</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_55_2_Timoteo.html">Seconda lettera a Timoteo</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_56_Tito.html">Lettera a Tito</a>\
+                            </li>\
+                            <li>\
+                                <a href="NR06_57_Filemone.html">Lettera a Filemone</a>\
+                            </li>\
+                        </ul>\
+                    </li>\
+                    <li class="sub-dropdown">\
+                        <div>\
+                            <span>Altre lettere apostoliche</span>\
+                            <span class="material-symbols-outlined">\
+                                chevron_right\
+                            </span>\
+                        </div>\
+                        <ul class="sub-menu">\
+                            <li>\
+                                <a href="Altre_lettere.html"><h3>Altre lettere</h3></a>\
+                            </li>\
+                            <li>\
+                            <a href="NR06_58_Ebrei.html">Lettera agli Ebrei</a>\
+                            </li>\
+                            <li>\
+                            <a href="NR06_59_Giacomo.html">Lettera di Giacomo</a>\
+                            </li>\
+                            <li>\
+                            <a href="NR06_60_1_Pietro.html">Prima lettera di Pietro</a>\
+                            </li>\
+                            <li>\
+                            <a href="NR06_61_2_Pietro.html">Seconda lettera di Pietro</a>\
+                            </li>\
+                            <li>\
+                            <a href="NR06_62_1_Giovanni.html">Prima lettera di Giovanni</a>\
+                            </li>\
+                            <li>\
+                            <a href="NR06_63_2_Giovanni.html">Seconda lettera di Giovanni</a>\
+                            </li>\
+                            <li>\
+                            <a href="NR06_64_3_Giovanni.html">Terza lettera di Giovanni</a>\
+                            </li>\
+                            <li>\
+                            <a href="NR06_65_Giuda.html">Lettera di Giuda</a>\
+                            </li>\
+                            <li>\
+                            <a href="NR06_66_Apocalisse.html">Apocalisse di Giovanni</a>\
+                            </li>\
+                        </ul>\
+                    </li>\
+                </ul>\
+            </li>\
+        </ul>\
+    </div>\
+    <div class="buttons">\
+    <a href="001Lavorazione_Bibbia_HTML">\
+      <button class="btn">HTML</button>\
+    </a>\
+    </div>\
+</nav>';
+
+}
